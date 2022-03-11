badeb3cc47e32daa546474d769589fa91c25e38f
codeswith-noah <85592454+codeswith-noah@users.noreply.github.com>
2022-03-11 16:03:46 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
-var link = 'https://teachablemachine.withgoogle.com/models/H__K0li6y/model.json';
+var dog = 0;
+var cat = 0;
+var cow = 0;
 
 function startClassification(){
     navigator.mediaDevices.getUserMedia({audio:true});
-    classifier = ml5.soundClassifier(link, modelReady);
+    classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/H__K0li6y/model.json', modelReady);
 }
 
 function modelReady(){
@@ -15,6 +17,32 @@ function gotResults(error, results){
     }
     else{
         console.log(results);
+        random_number_r = Math.floor(Math.random() * 255) + 1;
+        random_number_g = Math.floor(Math.random() * 255) + 1;
+        random_number_b = Math.floor(Math.random() * 255) + 1;
+
+        document.getElementById("result_label").innerHTML = 'I can hear - ' + results[0].label;
+        document.getElementById("result_confidence").innerHTML = 'I can hear - ' + (results[0].confidence*100).toFixed(2)+"%";
+        document.getElementById("result_label").style.color = "rgb(" + random_number_r + random_number_g + random_number_b + ")";
+        document.getElementById("result_label").style.color = "rgb(" + random_number_r + random_number_g + random_number_b + ")";
+
+        img = document.getElementById("resultimg");
+
+        if(results[0].label == "dog"){
+            dog = dog + 1;
+            img.src("https://thehappypuppysite.com/wp-content/uploads/2019/03/Cartoon-Dog-Names-HP-long.jpg");
+        }
+        else if(results[0].label == "cat"){
+            cat = cat + 1;
+            img.src("https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/Orange_cat_cartoon.png/640px-Orange_cat_cartoon.png");
+        }
+        else if(results[0].label == "cow"){
+            cow = cow + 1;
+            img.src("https://media.istockphoto.com/vectors/vector-illustration-of-cartoon-cow-vector-id629340092?k=20&m=629340092&s=170667a&w=0&h=gmz8ngtthbwBGVDaP3v1ji7uyzzO223GGtxAg_gY8Oo=");
+        }
+        else{
+            img.src("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSo5K3p-lVM73CM_TCsZJfYAt-qODROcH4MGV3WsgEFgmNk1B5JfBHZERExuXcSkX7MN1E&usqp=CAU");
+        }
     }
 }
 
